6f8ffe3890bdf75724fc9f90c1a636ca44904e79
Mikhail Khramtsov <57592116+hramtsov13@users.noreply.github.com>
2022-02-18 20:43:11 +0500

    Fix/router (#5)
    * fix(router): i18n test
    
    * fix(router): fixed route issue when refreshing student view

--- frontend/src/views/StudentView.vue
+++ frontend/src/views/StudentView.vue
@@ -1,33 +1,35 @@
 <template>
-  <div>
-    {{ `${studentToShow.personalInfo.name} ${studentToShow.personalInfo.lastName}` }}
-    <p v-if="studentToShow.lessons.length">Учитель: {{ studentToShow.lessons[0].teacher }}</p>
-    <p v-if="studentToShow.personalInfo.comment">
-      Комментарий: {{ studentToShow.personalInfo.comment }}
+  <div v-if="studentToShow" class="text-black">
+    <div class="gap-x-2 flex items-center justify-between mb-4 font-medium">
+      <p class="text-student_name">
+        {{ `${studentToShow.personalInfo.name} ${studentToShow.personalInfo.lastName}` }}
+      </p>
+      <p class="text-lg">
+        {{ $t('STUDENT_VIEW_INFO_LANGUAGE_LEVEL') }}:
+        <span class="text-orange">{{ studentToShow.personalInfo.languageLevel }}</span>
+      </p>
+    </div>
+    <p v-if="studentToShow.lessons.length">
+      {{ $t('STUDENT_VIEW_INFO_TEACHER') }}: {{ studentToShow.lessons[0].teacher }}
     </p>
-    <p>Телефон: {{ studentToShow.personalInfo.phone }}</p>
-    <p>E-mail: {{ studentToShow.personalInfo.email }}</p>
-    <p>Уровень языка: {{ studentToShow.personalInfo.languageLevel }}</p>
+    <p v-if="studentToShow.personalInfo.comment">
+      {{ $t('STUDENT_VIEW_INFO_COMMENT') }}: {{ studentToShow.personalInfo.comment }}
+    </p>
+    <p>{{ $t('STUDENT_VIEW_INFO_PHONE') }}: {{ studentToShow.personalInfo.phone }}</p>
+    <p>{{ $t('STUDENT_VIEW_INFO_EMAIL') }}: {{ studentToShow.personalInfo.email }}</p>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 import { useUsersStore } from '../stores/useUsers'
 import { IStudent } from '../helpers/studentsMocks'
 
 const store = useUsersStore()
 const route = useRoute()
-const studentToShow = ref({})
 
-const getStudent = () => {
-  store.$state.students.forEach((student: IStudent) => {
-    if (String(student.id) === route.params.id) {
-      studentToShow.value = { ...student }
-    }
-  })
-}
-
-getStudent()
+const studentToShow = computed(() =>
+  store.$state.students.find((student: IStudent) => String(student.id) === route.params.id)
+)
 </script>
